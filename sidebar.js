af0bf23a2b367979990540b3095ab2f4e21d2b80
Harsh Gaur <gaurharsh336@gmail.com>
2026-09-08 17:41:22 +0530

    SideBar file addition
    SideBar file addition

---
+++ sidebar.js
@@ -0,0 +1,28 @@
+const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+
+const navigationItems = [
+  ['index.html', 'home'],
+  ['work.html', 'work'],
+  ['create.html', 'create'],
+  ['about.html', 'about'],
+  ['contact.html', 'contact']
+];
+
+const navigationMarkup = navigationItems.map(([href, label]) => {
+  const active = currentPage === href;
+  return `<li><a href="${href}"${active ? ' class="active" aria-current="page"' : ''}>${label}</a></li>`;
+}).join('');
+
+document.querySelector('#sidebar').innerHTML = `
+  <nav class="sidebar">
+    <div class="avatar">HG</div>
+    <div class="mark">Harsh Gaur</div>
+    <div class="status"><span class="dot"></span>Associate Consultant, Capgemini</div>
+    <ul class="filetree">
+      ${navigationMarkup}
+    </ul>
+    <div class="sidebar-foot">
+      Haryana, India
+    </div>
+  </nav>
+`;
